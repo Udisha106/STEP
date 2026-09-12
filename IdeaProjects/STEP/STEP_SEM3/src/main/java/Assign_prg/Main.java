c88367e221c5828c12b3bf5f31b22c60129d0041
Udisha Srivastava <udishasrivastava106@gamil.com>
2026-09-13 01:14:33 +0530

    Added assignment 1

---
+++ IdeaProjects/STEP/STEP_SEM3/src/main/java/Assign_prg/Main.java
@@ -0,0 +1,17 @@
+package Assign_prg;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DuplicateSeatChecker.run();
+
+        TypingAccuracyChecker.run();
+
+        TrafficSignalAnalyzer.run();
+
+        InventoryBalancer.run();
+
+        MovieReviewProfiler.run();
+    }
+}
---
+++ IdeaProjects/STEP/STEP_SEM3/src/main/java/Assign_prg/TrafficSignalAnalyzer.java
@@ -0,0 +1,36 @@
+package Assign_prg;
+
+public class TrafficSignalAnalyzer {
+
+    static void findLongestStreak(String signalLog) {
+
+        int currentStreak = 1;
+        int longestStreak = 1;
+
+        char longestColor = signalLog.charAt(0);
+
+        for (int i = 1; i < signalLog.length(); i++) {
+
+            if (signalLog.charAt(i) == signalLog.charAt(i - 1)) {
+                currentStreak++;
+            } else {
+                currentStreak = 1;
+            }
+
+            if (currentStreak > longestStreak) {
+                longestStreak = currentStreak;
+                longestColor = signalLog.charAt(i);
+            }
+        }
+
+        System.out.println("Longest Streak: '" + longestColor +
+                "' repeated " + longestStreak + " times");
+    }
+
+    static void run() {
+        String signalLog = "RRGGGYRR";
+
+        System.out.println("\n--- Traffic Signal Streak Analyzer ---");
+        findLongestStreak(signalLog);
+    }
+}
